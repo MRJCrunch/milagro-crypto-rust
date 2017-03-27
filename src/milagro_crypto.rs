b6bb7ee97c8b5d87e59b94e2b07c474a9a253bd0
Sergej Pupykin <sergej.pupykin@dsr-company.com>
2017-03-27 20:02:34 +0300

    fix i/o test and add prototypes

--- src/milagro_crypto.rs
+++ src/milagro_crypto.rs
@@ -7,6 +7,10 @@ pub struct BigNum {
 }
 
 impl BigNum {
+
+    /*
+     * New
+     */
     pub fn new(n: usize) -> BigNum {
         let mut arr = Vec::<BIG>::with_capacity(n);
         for i in 0..n {
@@ -18,6 +22,9 @@ impl BigNum {
         }
     }
 
+    /*
+     * from_bytes
+     */
     pub fn from_bytes(val: &[u8], size: usize) -> BigNum {
         let blen = (size+31)/32;
         let mut ret = BigNum::new(blen);
@@ -28,9 +35,45 @@ impl BigNum {
         return ret;
     }
 
+    /*
+     * to_string
+     */
     pub fn to_string(&mut self) -> String {
         return ff_to_string(self.storage.as_mut_slice(), self.size as i32);
     }
+
+    /*
+     * add
+     * self += val
+     */
+    pub fn add(&mut self, val: BigNum) {
+        // TODO: implement
+    }
+
+    /*
+     * sub
+     * self -= val
+     */
+    pub fn sub(&mut self, val: BigNum) {
+        // TODO: implement
+    }
+
+    /*
+     * mul
+     * self *= val
+     */
+    pub fn mul(&mut self, val: BigNum) {
+        // TODO: implement
+    }
+
+    /*
+     * TODO: fn list
+     *  pub fn FF_random(x: &mut BIG, R: &mut csprng, n: c_int) -> c_void;
+     *  pub fn FF_mod(x: &mut BIG, m: &mut BIG, n: c_int) -> c_void;
+     *  pub fn FF_sqr(x: &mut BIG, y: &mut BIG, n: c_int) -> c_void;
+     *  pub fn FF_pow(r: &mut BIG, x: &mut BIG, e: &mut BIG, m: &mut BIG, n: c_int) -> c_void;
+     *  pub fn FF_prime(x: &mut BIG, R: &mut csprng, n: c_int) -> c_int;
+     */
 }
 
 #[cfg(test)]
@@ -39,11 +82,13 @@ mod tests {
 
     #[test]
     fn test_bignum_io() {
-        let val: [u8; 32] = [ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03 ];
+        let val: [u8; 32] = [ 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+                              0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00,
+                              0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+                              0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00 ];
         let mut x = BigNum::from_bytes(&val[0..], 32);
-        println!("bignum_io: str = {}", x.to_string());
+        let str = x.to_string();
+        println!("bignum_io: str = {}", str);
+        assert_eq!(str, "112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF00");
     }
 }
