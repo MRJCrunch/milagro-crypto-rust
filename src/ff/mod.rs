73596f86136aed03f337b99a3e87b9c0da18ed5d
artem.ivanov <artem.ivanov@dsr-company.com>
2017-04-06 17:08:43 +0300

    Overloading add, sub, mul functions

--- src/ff/mod.rs
+++ src/ff/mod.rs
@@ -270,6 +270,31 @@ impl fmt::Display for FF {
     }
 }
 
+use std::ops::{Add, Mul, Sub};
+
+impl<'a, 'b> Add<&'b FF> for &'a FF {
+    type Output = FF;
+
+    fn add(self, other: &'b FF) -> FF {
+        FF::add(self, other)
+    }
+}
+
+impl<'a, 'b> Mul<&'b FF> for &'a FF {
+    type Output = FF;
+
+    fn mul(self, other: &'b FF) -> FF {
+        FF::mul(self, other)
+    }
+}
+
+impl<'a, 'b> Sub<&'b FF> for &'a FF {
+    type Output = FF;
+
+    fn sub(self, other: &'b FF) -> FF {
+        FF::sub(self, other)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -453,5 +478,44 @@ mod tests {
         let bv = FF::from_bytes(&bytes[0..], bigsize*MODBYTES, 0);
         let r = FF::randomnum(&bv, &mut rng);
         println!("ff_randomN: bsize = {}, bigsize = {}, bv = {}, r = {}", bsize, bigsize, bv, r);
+    }
+
+    #[test]
+    fn plus_test() {
+        let x = FF::from_hex("1", 0);
+        let y = FF::from_hex("2", 0);
+        let z = &x + &y;
+        let str = z.to_hex();
+        println!("ff_mul: x = {}", &x);
+        println!("ff_mul: x = {}", &y);
+        println!("ff_mul: str = {}", str);
+        assert_eq!(str, "0000000000000000000000000000000000000000000000000000000000000000 \
+                         0000000000000000000000000000000000000000000000000000000000000003");
+    }
+
+    #[test]
+    fn sub_test() {
+        let x = FF::from_hex("2", 0);
+        let y = FF::from_hex("1", 0);
+        let z = &x - &y;
+        let str = z.to_hex();
+        println!("ff_mul: x = {}", &x);
+        println!("ff_mul: x = {}", &y);
+        println!("ff_mul: str = {}", str);
+        assert_eq!(str, "0000000000000000000000000000000000000000000000000000000000000000 \
+                         0000000000000000000000000000000000000000000000000000000000000001");
+    }
+
+    #[test]
+    fn mul_test() {
+        let x = FF::from_hex("2", 0);
+        let y = FF::from_hex("1", 0);
+        let z = &x * &y;
+        let str = z.to_hex();
+        println!("ff_mul: x = {}", &x);
+        println!("ff_mul: x = {}", &y);
+        println!("ff_mul: str = {}", str);
+        assert_eq!(str, "0000000000000000000000000000000000000000000000000000000000000000 \
+                         0000000000000000000000000000000000000000000000000000000000000002");
     }
 }
